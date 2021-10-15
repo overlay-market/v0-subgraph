3d7b540ec841f13f022c0418547730baeba0cec9
realisation <realisation@protonmail.com>
2021-10-15 18:51:03 -0400

    constructed first partof liquidation price update - looping markets comparing compoundability

--- src/OverlayV1UniswapV3Market.ts
+++ src/OverlayV1UniswapV3Market.ts
@@ -1,12 +1,15 @@
-import { ethereum, log } from "@graphprotocol/graph-ts"
+import { Address, ethereum, log } from "@graphprotocol/graph-ts"
 
 import {
   FundingPaid,
   NewPrice,
+  OverlayV1UniswapV3Market,
 } from "../generated/OverlayV1UniswapV3Market/OverlayV1UniswapV3Market"
 
 import {
-  loadMarket
+  loadMarket,
+  loadMarketManifest,
+  loadMarketMonitor,
 } from "./utils"
 
 export function handleFundingPaid(event: FundingPaid): void { }
@@ -18,6 +21,38 @@ export function handleNewPrice(event: NewPrice): void {
 }
 
 export function handleBlock(block: ethereum.Block): void {
+
+
+  let now = block.timestamp
+
+  let manifest = loadMarketManifest()
+
+  let compoundings = manifest.compoundings
+
+  let markets = manifest.markets
+
+  for (let i = 0; i < compoundings.length; i++) {
+
+    let compounding = compoundings[i]
+    let marketAddr = markets[i]
+
+    if (compounding.le(now)) {
+
+      let market = loadMarket(Address.fromByteArray(marketAddr) as Address)
+
+      let oi = OverlayV1UniswapV3Market.bind(Address.fromByteArray(marketAddr) as Address).oi()
+
+      let monitor = loadMarketMonitor(Address.fromByteArray(marketAddr) as Address)
+
+      let positions = monitor.positions
+
+      for (let j = 0; j < positions.length; j++) {
+
+      }
+
+    }
+
+  }
 
   log.info("\n\nhandling the block number {} time {}\n\n", [block.number.toString(), block.timestamp.toString()])
 

491ee4c03e2b4af6df2a18469856893003806150
realisation <realisation@protonmail.com>
2021-10-14 22:55:07 -0400

    create market, manifest and monitor

--- src/OverlayV1UniswapV3Market.ts
+++ src/OverlayV1UniswapV3Market.ts
@@ -1,13 +1,70 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt, ethereum, log } from "@graphprotocol/graph-ts"
+import { OverlayV1OVLCollateral__marketInfoResult } from "../generated/OverlayV1OVLCollateral/OverlayV1OVLCollateral"
 import {
   OverlayV1UniswapV3Market,
   FundingPaid,
   NewPrice,
-  log
 } from "../generated/OverlayV1UniswapV3Market/OverlayV1UniswapV3Market"
+import {
+  Market,
+  MarketManifest,
+  MarketMonitor
+} from "../generated/schema"
+
+function monitorMarket (_market: Address): void {
+
+  let market = OverlayV1UniswapV3Market.bind(_market)
+
+  let manifest = MarketManifest.load('1')
+  if (manifest == null) manifest = new MarketManifest('1')
+
+  let markets = manifest.markets
+  let compoundings = manifest.compoundings
+
+  let compounded = market.compounded()
+  let compoundPeriod = market.compoundingPeriod()
+
+  markets.push(market._address)
+  compoundings.push(compounded.plus(compoundPeriod))
+
+  manifest.markets = markets
+  manifest.compoundings = compoundings
+
+  manifest.save()
+
+
+}
+
+function getMarket (address: Address): Market {
+
+  let market = Market.load(address.toHex())
+
+  if (market == null) {
+
+    monitorMarket(address)
+
+    market = new Market(address.toHex())
+    market.save()
+
+    let monitor = new MarketMonitor(address.toHex())
+    monitor.save()
+
+  }
+
+  return market
+
+}
 
 export function handleFundingPaid(event: FundingPaid): void { }
 
-export function handleNewPrice(event: NewPrice): void {}
+export function handleNewPrice(event: NewPrice): void {
 
-export function handlelog(event: log): void {}
+  let market = getMarket(event.address)
+
+}
+
+export function handleBlock(block: ethereum.Block): void {
+
+  log.info("\n\nhandline the block\n\n", [])
+
+}
